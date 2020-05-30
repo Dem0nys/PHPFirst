b66deae3db61f644102c5b8e8e489ca62577c430
Dem0nys <dmitromart@gmail.com>
2020-05-30 11:59:46 +0000

    somd

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 define('DB_NAME','lomick');
 define('DB_USER','lomi');
 define('DB_PASSWORD','1769b5fe84ef4711af78775c267f54a6');
-define('DB_HOST','192.168.0.120');
-//define('DB_HOST','mysql');
+//define('DB_HOST','192.168.0.120');
+define('DB_HOST','mysql');
